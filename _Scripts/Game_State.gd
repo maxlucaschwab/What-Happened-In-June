8834fefeb26497159e8d49c0cc4e68f66b345285
maxlucaschwab <114507212+maxlucaschwab@users.noreply.github.com>
2024-05-28 22:56:05 -0700

    camera movement changes, object tracking

--- _Scripts/Game_State.gd
+++ _Scripts/Game_State.gd
@@ -1,3 +1,6 @@
 extends Node
 
 var test_bool: bool = false
+var inDialogue: bool = false
+var index = [[-184.513, -160.25], [-984,-393], [916,112]]
+var position
--- _Scripts/Main.gd
+++ _Scripts/Main.gd
@@ -3,11 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	$Cam_area/CollisionShape2D/Camera2D.position = get_local_mouse_position()
-	pass
+	if GameState.inDialogue == false:
+		$Cam_area/CollisionShape2D/Camera2D.position = get_local_mouse_position()
+	else: 
+		$Cam_area/CollisionShape2D/Camera2D.position = Vector2(GameState.position[0], GameState.position[1])
